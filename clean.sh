44e4d1aaeba683532fe9b134c2d0d06845e6dbb8
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2014-11-01 18:36:08 +0100

    Add clean script to delete compiled packages
    Cleaning directories before compiling packages (with makepkg) or
    preparing AUR tarball (with mkaurball) eases some maintenance steps like
    package verification (with namcap) and AUR upload (with burp).

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Clean all compiled packages and AUR source tarballs
+# This does NOT clean downloaded files.  To find these files, you can use:
+#    git ls-files --ignored --others --exclude-standard
+
+# Ensure current directory is the top dir
+cd "$(dirname "$0")"
+
+rm -frv */src */pkg
+rm -fv */*.pkg.tar.xz
+rm -fv */*.pkg.tar.xz.sig
+rm -fv */*.src.tar.gz
