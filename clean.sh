4ce552f5ce904d5979bad4d5bc260c03873c1d19
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2015-08-07 17:24:37 +0800

    clean.sh: increase script robustness

--- clean.sh
+++ clean.sh
@@ -4,9 +4,12 @@
 #    git ls-files --ignored --others --exclude-standard
 
 # Ensure current directory is the top dir
-cd "$(dirname "$0")"
+cd "$(dirname -- "$0")"
 
-rm -frv */src */pkg
-rm -fv */*.pkg.tar.xz
-rm -fv */*.pkg.tar.xz.sig
-rm -fv */*.src.tar.gz
+rm -frv ./*/src/ ./*/pkg/
+rm -fv ./*/*.pkg.tar.xz
+rm -fv ./*/*.pkg.tar.xz.sig
+rm -fv ./*/*.src.tar.gz
+
+# Also clean the downloaded base PKGBUILDs
+rm -frv base-noselinux/
