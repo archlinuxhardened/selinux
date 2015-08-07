1df270488c5077c8ccec215b0d664b050601a511
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2015-08-07 17:24:50 +0800

    Add a helper script to create AUR4 commits

---
+++ commit_update.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Commit local modifications to prepare the update of AUR packages
+
+cd "$(dirname -- "$0")" || exit $?
+
+# Run the given command after displaying it
+log_and_run() {
+    echo "Running: $*"
+    "$@"
+}
+
+# reset the git repository
+log_and_run git reset HEAD
+
+for DIR in $(find . -maxdepth 2 -name PKGBUILD -printf '%h\n' | sort)
+do
+    PKGNAME="${DIR##*/}"
+
+    # Ignore directories without any change
+    [ -n "$(git status --porcelain "$DIR")" ] || continue
+
+    # Update .SRCINFO
+    log_and_run mksrcinfo -o "$DIR/.SRCINFO" "$DIR/PKGBUILD" || exit $?
+
+    # Commit everything with a custom commit message
+    log_and_run git add "$DIR"
+    PKGVER="$(sed -n 's/^\s*pkgver = \(.*\)$/\1/p' "$DIR/.SRCINFO" | head -n1)"
+    PKGREL="$(sed -n 's/^\s*pkgrel = \(.*\)$/\1/p' "$DIR/.SRCINFO" | head -n1)"
+    log_and_run git commit -m "$PKGNAME $PKGVER-$PKGREL update"
+    tput bold
+    echo "$PKGNAME changes has been commited. You can now push them to the AUR with:"
+    echo "git subtree push --prefix=${DIR#./} aur-$PKGNAME master"
+    tput sgr0
+done
