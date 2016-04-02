663bf0dae3144b5e77145538c4aed1ac6332196e
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2016-04-02 12:05:50 +0200

    sync_srcinfo: simplify code with makepkg 5.0.1
    Since commit
    https://projects.archlinux.org/pacman.git/commit/?id=f63854fa96f658ca5bdf2c21a1cd33cf4e3fbdbd
    makepkg --printsrcinfo no longer includes an anonying comment header.
    The code which has been introduced to take care of it is thus no longer
    needed.
    
    While at it, sort the package list when processing them.

--- sync_srcinfos.sh
+++ sync_srcinfos.sh
@@ -5,19 +5,9 @@ set -e
 
 cd "$(dirname -- "$0")"
 
-find . \( -name base-noselinux -prune \) -o -name PKGBUILD -printf '%h\n' | \
+find . \( -name base-noselinux -prune \) -o -name PKGBUILD -printf '%h\n' | sort | \
 while read -r DIR
 do
     echo "Generating $DIR/.SRCINFO"
-    # As the header comment may differ (it contains a date and makepkg
-    # version), compare without and update if other things differ.
-    (cd "$DIR" && makepkg --printsrcinfo > .SRCINFO.new)
-    if diff -q \
-        <(sed -e '1,2{/^#/d}' "$DIR/.SRCINFO") \
-        <(sed -e '1,2{/^#/d}' "$DIR/.SRCINFO.new")
-    then
-        rm "$DIR/.SRCINFO.new"
-    else
-        mv "$DIR/.SRCINFO.new" "$DIR/.SRCINFO"
-    fi
+    (cd "$DIR" && makepkg --printsrcinfo > .SRCINFO)
 done
