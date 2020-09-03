bd9b356652cf76cb6d1e5b26d935dc998ec00894
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2020-09-03 22:18:06 +0200

    build_and_install_all: do not run systemd-selinux's checks in a container
    Currently systemd's tests fail in an unprivileged podman container:
    
        281/504 test-condition                            FAIL           0.53s (killed by signal 6 SIGABRT)
        288/504 test-util                                 FAIL           0.43s (killed by signal 6 SIGABRT)
        299/504 test-fs-util                              FAIL           0.42s (killed by signal 6 SIGABRT)
        302/504 test-fd-util                              FAIL           1.28s (killed by signal 6 SIGABRT)
        310/504 test-specifier                            FAIL           0.37s (killed by signal 6 SIGABRT)
        331/504 test-acl-util                             FAIL           0.69s (killed by signal 6 SIGABRT)
        375/504 test-strv                                 FAIL           0.43s (killed by signal 6 SIGABRT)
        377/504 test-path                                 FAIL           30.44s (exit status 1)
        390/504 test-id128                                FAIL           0.47s (killed by signal 6 SIGABRT)
        441/504 test-dhcp-client                          FAIL           2.58s (killed by signal 6 SIGABRT)
        446/504 test-dhcp6-client                         FAIL           0.42s (killed by signal 6 SIGABRT)
    
    For example test-acl-util fails because setting ACL on files is
    forbidden in the default Seccomp filter.

--- build_and_install_all.sh
+++ build_and_install_all.sh
@@ -61,6 +61,13 @@ build() {
     rm -rf "./$1/src" "./$1/pkg"
     rm -f "./$1/"*.pkg.tar.xz "./$1/"*.pkg.tar.xz.sig
     rm -f "./$1/"*.pkg.tar.zst "./$1/"*.pkg.tar.zst.sig
+    # When building in a container, systemd's tests fail because of default Seccomp filters
+    if [ "$1" = 'systemd-selinux' ] && \
+        (systemd-detect-virt -q --container || systemd-detect-virt -q --private-users ) && \
+        grep '^Seccomp:\s*2$' /proc/self/status > /dev/null
+    then
+        set -- "$@" --nocheck
+    fi
     (cd "./$1" && shift && makepkg -s -C --noconfirm "$@") || exit $?
 }
 
