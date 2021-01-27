da323dac51f9cd2c132423e103d2395447177d13
Nicolas Iooss <nicolas.iooss_git@polytechnique.org>
2021-01-27 10:31:42 +0100

    Remove the blank line introduced by updpkgsum in some PKGBUILD files

--- sync_srcinfos.sh
+++ sync_srcinfos.sh
@@ -10,4 +10,11 @@ while read -r DIR
 do
     echo "Generating $DIR/.SRCINFO"
     (cd "$DIR" && updpkgsums && makepkg --printsrcinfo > .SRCINFO)
+
+    # For base-selinux and base-devel-selinux, updpkgsums introduces an blank
+    # line at the end of the file. Delete it.
+    if [ "$DIR" = ./base-devel-selinux ] || [ "$DIR" = ./base-selinux ]
+    then
+        sed '${/^$/d}' -i "$DIR/PKGBUILD"
+    fi
 done
